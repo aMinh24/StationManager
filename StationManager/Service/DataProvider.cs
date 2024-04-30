544588ff8a05bc45b9b32457c5f9daf71940a820
Ho Anh Minh <yamchahuyvs@gmail.com>
2024-04-30 11:39:53 +0700

    [add] DataProvider

---
+++ StationManager/Service/DataProvider.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationManager.Service
+{
+    public class DataProvide
+    {
+        private static DataProvide instance;
+        public static DataProvide Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataProvide();
+                }
+                return instance;
+            }
+            private set { instance = value; }
+        }
+        private DataProvide() { }
+        string connectionStr = "Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=minhdtbz_TaskManager;User ID=minhdtbz_TaskManager;Password=minh;Persist Security Info=True;Encrypt=false;";
+        public bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionStr))
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public DataTable ExcuteQuery(string query, object[] parameter = null)
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string para in listPara)
+                    {
+                        if (para.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(para, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dataTable);
+                connection.Close();
+            }
+            return dataTable;
+        }
+        public int ExcuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string para in listPara)
+                    {
+                        if (para.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(para, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+
+        public object ExcuteScalar(string query, object[] parameter = null)
+        {
+            object data = null;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string para in listPara)
+                    {
+                        if (para.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(para, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = cmd.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+        }
+    }
+    public static class DataProviderExtension
+    {
+        public static DataTable ExcuteQuery(this object obj, string query, object[] parameter = null)
+        {
+            return DataProvide.Instance.ExcuteQuery(query, parameter);
+        }
+        public static int ExcuteNonQuery(this object obj, string query, object[] parameter = null)
+        {
+            return DataProvide.Instance.ExcuteNonQuery(query, parameter);
+        }
+        public static object ExcuteScalar(this object obj, string query, object[] parameter = null)
+        {
+            return DataProvide.Instance.ExcuteScalar(query, parameter);
+        }
+    }
+}
